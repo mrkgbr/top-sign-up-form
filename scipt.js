14535e13ed8331c9d90c17ba348bbbf6b9e2ff24
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-03-27 21:49:21 +0200

    Fixed password validation

--- scipt.js
+++ scipt.js
@@ -5,15 +5,16 @@ const feedback = document.querySelector("#pwd-feedback");
 password.addEventListener("input", passwordCheck);
 
 confPassword.addEventListener("input", passwordCheck);
-
 function passwordCheck() {
-  if (password.value !== confPassword.value) {
+  if (password.value == "" || confPassword.value == "") {
+    feedback.textContent = "Password is required.";
+  } else if (password.value !== confPassword.value) {
     feedback.classList.add("error");
     feedback.classList.remove("match");
     feedback.textContent = "Password does not match!";
   } else {
-    feedback.textContent = "Password does match!";
     feedback.classList.add("match");
     feedback.classList.remove("error");
+    feedback.textContent = "Password does match!";
   }
 }
